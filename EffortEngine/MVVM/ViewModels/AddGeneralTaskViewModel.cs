39d6fd2d0b0a44f9f8e5ba61e165b138de664466
Rafał Marcinkowski <rafal.marcinkowski94@gmail.com>
2025-04-11 16:49:04 +0200

    Further UI updates

--- EffortEngine/MVVM/ViewModels/AddGeneralTaskViewModel.cs
+++ EffortEngine/MVVM/ViewModels/AddGeneralTaskViewModel.cs
@@ -1,34 +1,63 @@
 ﻿using DataAccess.Data;
+using EffortEngine.LocalLibrary;
+using MahApps.Metro.Controls.Dialogs;
 
 namespace EffortEngine.MVVM.ViewModels;
 
-public class AddGeneralTaskViewModel : BindableBase
+public class AddGeneralTaskViewModel(ITaskData taskData, TaskManager taskManager) : BindableBase
 {
-    private string generalTaskName;
-    public string GeneralTaskName
+    private string taskName = string.Empty;
+    public string TaskName
     {
-        get => generalTaskName;
-        set => SetProperty(ref generalTaskName, value);
+        get => taskName;
+        set => SetProperty(ref taskName, value);
     }
-    private string generalTaskDescription = string.Empty;
-    public string GeneralTaskDescription
+
+    private string taskDescription = string.Empty;
+    public string TaskDescription
     {
-        get => generalTaskDescription;
-        set => SetProperty(ref generalTaskDescription, value);
+        get => taskDescription;
+        set => SetProperty(ref taskDescription, value);
     }
-    private string selectedPriority = string.Empty;
-    private readonly ITaskData taskData;
-    public AddGeneralTaskViewModel(ITaskData taskData)
+
+    private string taskPriority = string.Empty;
+    public string TaskPriority
     {
-        this.taskData = taskData;
+        get => taskPriority;
+        set => SetProperty(ref taskPriority, value);
     }
-    public string SelectedPriority
+
+    private int selectedTabIndex;
+    public int SelectedTabIndex
     {
-        get => selectedPriority;
-        set => SetProperty(ref selectedPriority, value);
+        get => selectedTabIndex;
+        set
+        {
+            if (SetProperty(ref selectedTabIndex, value))
+            {
+                HandleTabSelectionChanged();
+            }
+        }
     }
+
     public IAsyncCommand AddGeneralTaskCommand => new AsyncDelegateCommand(async () =>
     {
-
+        if (!await taskManager.TryAddTask(TaskName, TaskDescription, TaskPriority, SelectedTabIndex)) return;
+        await dialogCoordinator.ShowMessageAsync(this, "Dodano zadanie",
+           $"Nazwa: {TaskName}\n{TaskManager.TaskToAdd.Type} został dodany.");
+        await ClearFields();
     });
+
+    public async Task HandleTabSelectionChanged()
+    {
+        await Task.Delay(50);
+        await ClearFields();
+    }
+
+    private async Task ClearFields()
+    {
+        TaskPriority = null;
+        TaskName = null;
+        TaskDescription = null;
+    }
 }
